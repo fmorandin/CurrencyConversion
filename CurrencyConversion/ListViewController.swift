47080aff608e71a959ce0701f2c212428e13f2e1
Felipe Morandin <felipemorandin@gmail.com>
2024-08-06 20:02:53 +0100

    Remove Main.storyboard

--- CurrencyConversion/ListViewController.swift
+++ CurrencyConversion/ListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ListViewController.swift
 //  CurrencyConversion
 //
 //  Created by Felipe Morandin on 06/08/2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
